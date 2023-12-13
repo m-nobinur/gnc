b430b6826a71e7b146f7fe5d3a35fdaf72ce6f6b
M Nobinur <mohammadnobinur@gmail.com>
2023-12-13 06:05:31 +0600

    Update file

--- ex.ps1
+++
@@ -1,6 +0,0 @@
-$url = "https://raw.githubusercontent.com/m-nobinur/gnc/main/gnc.zip"
-$destination = "$env:USERPROFILE\Desktop\gnc.zip"
-Invoke-WebRequest -Uri $url -OutFile $destination
-$extractFolder = "$env:USERPROFILE\Desktop\gnc"
-Expand-Archive -Path $destination -DestinationPath $extractFolder -Force
-cd $extractFolder
